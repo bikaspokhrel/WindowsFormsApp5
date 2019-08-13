da948c1c19d802fbb60ea2b15d64c814dd56b6ed
Bikash Pokhrel <bpokhrel@aut.ac.nz>
2019-08-13 12:08:51 +1200

    Add project files.

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Hello
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGo = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lbxOutput = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnGo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnGo.Location = new System.Drawing.Point(261, 191);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(193, 82);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "Click Me";
+            this.btnGo.UseVisualStyleBackColor = false;
+            this.btnGo.Click += new System.EventHandler(this.BtnGo_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblOutput.Location = new System.Drawing.Point(261, 276);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(193, 35);
+            this.lblOutput.TabIndex = 3;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(261, 327);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(193, 22);
+            this.txtInput.TabIndex = 4;
+            // 
+            // lbxOutput
+            // 
+            this.lbxOutput.FormattingEnabled = true;
+            this.lbxOutput.ItemHeight = 16;
+            this.lbxOutput.Location = new System.Drawing.Point(493, 12);
+            this.lbxOutput.Name = "lbxOutput";
+            this.lbxOutput.Size = new System.Drawing.Size(295, 340);
+            this.lbxOutput.TabIndex = 5;
+            this.lbxOutput.SelectedIndexChanged += new System.EventHandler(this.LbxOutput_SelectedIndexChanged);
+            // 
+            // Hello
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbxOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnGo);
+            this.Name = "Hello";
+            this.Text = "Hello";
+            this.Load += new System.EventHandler(this.Hello_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.ListBox lbxOutput;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Hello : Form
+    {
+        int Age = 21;
+        public Hello()
+        {
+            InitializeComponent();
+        }
+
+        private void Hello_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnGo_Click(object sender, EventArgs e)
+        {
+            //lblOutput.Text = txtInput.Text; // The output after clicking the button will appear in the lable whose name is lblOutput.
+            //lbxOutput.Items.Add(txtInput.Text);
+            string Name = "Bikash Pokhrel"; // variable name is Name and type is string, value for the variable Name is Bikash Pokrel
+            Age = Age + 1;
+            lbxOutput.Items.Add(Name + " "+Age); // The listbox item is add as the variable
+            MessageBox.Show("Hello " + Name + " Welcome to C#, you are " +Age+" years old");
+     
+        }
+
+        private void LbxOutput_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lbxOutput.Items.Clear();
+        }
+    }
+}
